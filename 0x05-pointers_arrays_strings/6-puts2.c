0bcac452f12ff247f7ec674e528ee3eeabc85c07
okash1 <okashauwal21@gmail.com>
2022-09-19 17:18:30 -0700

    Should do

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,11 +11,11 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while ((*str + 1) != '\0')
+	while (*(str + i) != '\0')
 	{
 		if (i % 2 == 0)
 			_putchar(*(str + i));
-	i++;
+		i++;
 	}
 	_putchar(10);
 }
