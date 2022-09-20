8fa9d9d6d6a4431695d4d8fa291c5ddd2181a327
okash1 <okashauwal21@gmail.com>
2022-09-19 18:17:28 -0700

    finally

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,17 +9,18 @@
 
 void rev_string(char *s)
 {
-	int i, t, l = _strlen(s);	
+	int i, t, l = _strlen(s);
 
-	for (i = 0; i < l /2; i++)
+	for (i = 0; i < l / 2; i++)
 	{
 		t = *(s + i);
-		*(s + i) = *(s + l - i -1);
-		*(s + l -i -1) = t;
+		*(s + i) = *(s + l - i - 1);
+		*(s + l - i - 1) = t;
 	}
 }
+
 /**
-*_strlength - return the len of a str
+*_strlen - return the len of a str
 *@s: string
 *Return: the len of the given str
 */
@@ -27,7 +28,7 @@ void rev_string(char *s)
 int _strlen(char *s)
 {
 	int l = 0;
-	
+
 	while (*(s + l) != '\0')
 		l++;
 
